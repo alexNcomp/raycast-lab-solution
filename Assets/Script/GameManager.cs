d1bc7c4684af1ff1fcd206dfc39cdd370c2f71a1
alexNcomp <alexbuinguyen@gmail.com>
2023-02-26 22:58:54 -0500

    solution

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance { get; private set; }
+
+    // Write down your variables here
+    public float Score = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(Instance);
+    }
+
+    public void IncrementScore()
+    {
+        Score += 1;
+        Debug.Log(Score);
+        if (Score == 4)
+        {
+            Debug.Log("You win!");
+            SceneManager.LoadScene("NewScene");
+        }
+    }
+}
